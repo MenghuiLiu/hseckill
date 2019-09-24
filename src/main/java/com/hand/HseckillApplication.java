a76119615c5c73d2bf3ea0895c1e80d809c79fc7
刘梦辉 <menghui.liu@hand-china.com>
2019-09-24 09:14:59 +0800

    [INIT] 初始化项目

---
+++ src/main/java/com/hand/HseckillApplication.java
@@ -0,0 +1,18 @@
+package com.hand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import io.choerodon.resource.annoation.EnableChoerodonResourceServer;
+
+@EnableChoerodonResourceServer
+@EnableDiscoveryClient
+@SpringBootApplication
+public class HseckillApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HseckillApplication.class, args);
+    }
+}
+
+
