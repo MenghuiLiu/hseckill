9a8fdd8f023d4ff946b16449d25ef2871c842db1
刘梦辉 <menghui.liu@hand-china.com>
2019-09-26 00:10:11 +0800

    [IMP] 后端环境搭建

--- src/main/java/com/hand/HseckillApplication.java
+++ src/main/java/com/hand/HseckillApplication.java
@@ -2,12 +2,11 @@ package com.hand;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import io.choerodon.resource.annoation.EnableChoerodonResourceServer;
 
 //@EnableChoerodonResourceServer
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 @SpringBootApplication
+//@MapperScan("com.hand.infra.mapper")
 public class HseckillApplication {
 
     public static void main(String[] args) {
